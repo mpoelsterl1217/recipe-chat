fefcfbcc0018993e52549a694560063133ada567
Nicerrika <runkai.qiu@stonybrook.edu>
2024-11-18 15:07:56 -0600

    update 1,2,4,5,6

--- interface.py
+++ interface.py
@@ -1,5 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
+import nltk
+from nltk import word_tokenize, pos_tag
+import re
 
 def grab_info(response):
     # todo
@@ -8,11 +11,17 @@ def grab_info(response):
     ingredients_list = []
     for i in ingredients:
         ingredients_list.append(i.text)
+    # ingredients_list.pop(0)
+    ingredients_list = [item.strip() for item in ingredients_list]
+    ingredients_list = [item for item in ingredients_list if item != ""]   
     
     steps=soup.find_all(class_='comp mntl-sc-block mntl-sc-block-html')
     steps_list=[]
     for i in steps:
         steps_list.append(i.text)
+    # steps_list.pop(0)
+    steps_list = [item.strip() for item in steps_list]
+    steps_list = [item for item in steps_list if item != ""] 
     
     return ingredients_list, steps_list
 
@@ -28,9 +37,30 @@ def conform_url(url):
     except requests.ConnectionError:
         return False
 
+def extract_numbers_nltk(text):
+    tokens = word_tokenize(text)
+    pos_tags = pos_tag(tokens)
+    numbers = []
+    for word, tag in pos_tags:
+        if tag == "CD" or word.lower() in {"next", "previous", "repeat", "current"}:
+            numbers.append(word.lower())
+    return numbers
+
+def extract_number_re(text):
+    # Regex to match numbers at the beginning of the string
+    match = re.match(r"(\d+)", text)
+    if match:
+        return int(match.group(1))  # Convert to integer if needed
+    return -1
+
 def main():
+    input_history = []
+    respond_history = []
+    current_step = 0
+    current_ingredient = 0
     print("Please provide a valid AllRecipes.com URL or type 'q' to quit the program")
-    url = input()
+    # url = input()
+    url = "https://www.allrecipes.com/recipe/244765/easy-mexican-chicken-spaghetti/"
     if url.lower()=='q':
         print("Bye!")
         return
@@ -46,21 +76,106 @@ def main():
         while True:
             num=input()
             if num=='1':
-                print(ingredients_list)
+                print(ingredients_list[0])
+                input_history.append("1")
+                respond_history.append(ingredients_list[0])
+                current_ingredient = 0
                 break
             elif num=='2':
-                print(steps_list)
+                print(steps_list[0])
+                input_history.append("2")
+                respond_history.append(steps_list[0])
+                current_step = 0
                 break
             else:
                 print("error input, try again!")
         
         # todo understand what user input and response with what we get from websites
+        while True:
+            user_input=input()
+            print("cuurent input:", user_input)
+            input_history.append(user_input)
+            # 1 example
+            if "Show me the ingredients list" in user_input:
+                print(ingredients_list)
+                print()
+                respond_history.append(ingredients_list)
+                continue
+        
+            if "Show me the step list" in user_input:
+                print(steps_list)
+                print()
+                respond_history.append(steps_list)
 
-        user_input=input()
-        # 5 & 6. Simple "what is" questions. Specific "how to" questions
-        if "how" or "what" in user_input.lower():
-            reference_url="+".join(user_input.split(" "))
-            print("No worries. I found a reference for you: https://www.google.com/search?q=" + reference_url)
+            # 2 exmaple
+            num = extract_numbers_nltk(user_input)
+            if num and len(num)==1:
+                if "repeat" in num:
+                    print(steps_list[current_step])
+                    print()
+                    continue
+                if "step" in user_input:
+                    if "previous" in num:
+                        if current_step-1 < 0:
+                            print("no previous step")
+                            print()
+                            continue
+                        else:
+                            current_step -=1
+                            print(steps_list[current_step])
+                            print()
+                    elif "next" in num:
+                        if current_step + 1 >= len(steps_list):
+                            print("no next step")
+                            print()
+                            continue
+                        else:
+                            current_step +=1
+                            print(steps_list[current_step])
+                            print()
+                    elif "repeat" in num or "current" in num:
+                        print(steps_list[current_step])
+                        print()
+                    else:
+                        if extract_number_re(num[-1]) - 1 < 0 or extract_number_re(num[-1]) - 1 >= len(steps_list):
+                            print("no such step")
+                            print()
+                            continue
+                        else:
+                            current_step = extract_number_re(num[-1]) - 1
+                            print(steps_list[current_step])
+                            print()
+                    
+                    respond_history.append(steps_list[current_step])
+                    
+                    continue
+            
+            # 3 exmple
+
+
+
+            # 4 & 5 & 6. Simple "what is" questions. Specific "how to" questions
+            if "how to" in user_input.lower() or "what is" in user_input.lower():
+                # print("4,5,6")
+
+            # 6 use conversation history to infer what “that” refers to. may use SpaCy to deal with it.
+                if "how to" in user_input.lower() and ("that" in user_input.lower() or "this" in user_input.lower() or "these" in user_input.lower() or "those" in user_input.lower()):
+                    reference_url = respond_history[-1]
+                    reference_url="+".join(reference_url.split(" "))
+                    print("No worries. I found a reference for you: https://www.google.com/search?q=how+to+" + reference_url)
+                    print()
+                    continue
+            
+                reference_url="+".join(user_input.split(" "))
+                print("No worries. I found a reference for you: https://www.google.com/search?q=" + reference_url)
+                print()
+                continue
+            
+            # invaild input
+            print("Please provide an vaild input !")
+            print()
+
+            continue
         
 
     else:
--- test.py
+++ test.py
@@ -1,17 +1,55 @@
-import requests
-from bs4 import BeautifulSoup
+# import requests
+# from bs4 import BeautifulSoup
 
-response = requests.get("https://www.allrecipes.com/roasted-butternut-squash-and-spinach-lasagna-recipe-7563610")
-# response = requests.get("https://www.allrecipes.com/recipe/218091/classic-and-simple-meat-lasagna/")
+# response = requests.get("https://www.allrecipes.com/roasted-butternut-squash-and-spinach-lasagna-recipe-7563610")
+# # response = requests.get("https://www.allrecipes.com/recipe/218091/classic-and-simple-meat-lasagna/")
 
-# print(response.content)
+# # print(response.content)
 
-soup = BeautifulSoup(response.content, "html.parser")
-elements=soup.find_all(class_='mm-recipes-structured-ingredients__list-item')
+# soup = BeautifulSoup(response.content, "html.parser")
+# elements=soup.find_all(class_='mm-recipes-structured-ingredients__list-item')
 
-for i in elements:
-    print(i.text)
+# for i in elements:
+#     print(i.text)
 
-elements=soup.find_all(class_='comp mntl-sc-block mntl-sc-block-html')
-for i in elements:
-    print(i.text)
+# elements=soup.find_all(class_='comp mntl-sc-block mntl-sc-block-html')
+# for i in elements:
+#     print(i.text)
+
+
+# import nltk
+# from nltk import word_tokenize, pos_tag
+
+# def extract_numbers_nltk(text):
+#     tokens = word_tokenize(text)
+#     pos_tags = pos_tag(tokens)
+#     numbers = []
+#     for word, tag in pos_tags:
+#         if tag == "CD" or word.lower() in {"next", "previous"}:
+#             numbers.append(word)
+#     return numbers
+
+# # 示例
+# text = "We 1ST"
+# num = extract_numbers_nltk(text)
+# if num and len(num)==1:
+#     print("fuck")
+# print(extract_numbers_nltk(text))
+# # 输出: ['one', '2nd', 'previous']
+
+# import re
+
+import re
+
+def extract_number(text):
+    # Regex to match numbers at the beginning of the string
+    match = re.match(r"(\d+)", text)
+    if match:
+        return int(match.group(1))  # Convert to integer if needed
+    return None
+
+# Test cases
+examples = ["1st", "2nd", "3rd", "4th", "25th", "100th", "not-a-number", "1"]
+
+for example in examples:
+    print(f"{example} -> {extract_number(example)}")
